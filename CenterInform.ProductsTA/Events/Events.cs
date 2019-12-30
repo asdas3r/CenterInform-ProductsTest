8064fd5514304900988a8b4a4e84fd0c1f7deb5e
romanbk <r.bruk@outlook.com>
2019-12-30 18:16:51 +0300

    adding additional data

---
+++ CenterInform.ProductsTA/Events/Events.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterInform.ProductsTA.Events
+{
+    public class CloseTabEvent : PubSubEvent<object> { }
+}
